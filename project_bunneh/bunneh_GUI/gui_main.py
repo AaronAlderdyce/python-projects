05b3beae4d439e5b7ef6a5cb8054251a4caaa7de
Aaron Joseph Alderdyce <95669204+AaronAlderdyce@users.noreply.github.com>
2024-02-11 20:13:39 -0800

    tweaking GUI

--- project_bunneh/bunneh_GUI/gui_main.py
+++ project_bunneh/bunneh_GUI/gui_main.py
@@ -11,15 +11,18 @@ def draw_window():
     main_menu_frame = ctk.CTkFrame(window)
     main_menu_frame.pack(expand=True,fill="both")
 
-    button1 = ctk.CTkButton(window, text="New Game",)
+    text_box = ctk.CTkTextbox(main_menu_frame)
+    text_box.pack(expand=True,fill="both",)
+
+    button1 = ctk.CTkButton(main_menu_frame, text="New Game",)
     button1.pack(expand=True,fill="both", pady=1)
-    button2 = ctk.CTkButton(window, text="Load Game",)
+    button2 = ctk.CTkButton(main_menu_frame, text="Load Game",)
     button2.pack(expand=True,fill="both", pady=1)
-    button3 = ctk.CTkButton(window, text="Tutorial",)
+    button3 = ctk.CTkButton(main_menu_frame, text="Tutorial",)
     button3.pack(expand=True,fill="both", pady=1)
-    button4 = ctk.CTkButton(window, text="Credits",)
+    button4 = ctk.CTkButton(main_menu_frame, text="Credits",)
     button4.pack(expand=True,fill="both", pady=1)
-    button5 = ctk.CTkButton(window, text="Quit Game",)
+    button5 = ctk.CTkButton(main_menu_frame, text="Quit Game",)
     button5.pack(expand=True,fill="both", pady=1)
 
 
