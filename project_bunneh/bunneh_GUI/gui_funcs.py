fd2701cd01e81cf0806e8789b15b2530f0f3d5ca
Aaron Joseph Alderdyce <95669204+AaronAlderdyce@users.noreply.github.com>
2024-02-12 05:01:18 -0800

    got New Game and Quit Game to work.

---
+++ project_bunneh/bunneh_GUI/gui_funcs.py
@@ -0,0 +1,18 @@
+import customtkinter as ctk
+
+
+def new_char_window():
+    char_window = ctk.CTk()
+    char_window.title("Character Creation")
+    char_window.geometry("800x800")
+
+    # Frame for Char Creation
+    char_frame = ctk.CTkFrame(char_window)
+    char_frame.pack(expand=True,fill="both")
+
+    # Add buttons and widgets for char creation
+    label = ctk.CTkLabel(char_frame,text="Create Your Character:")
+    label.pack()
+
+    # Keeping window open
+    char_window.mainloop()
--- project_bunneh/bunneh_GUI/gui_main.py
+++ project_bunneh/bunneh_GUI/gui_main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-
+from gui_funcs import new_char_window
 
 def draw_window():
     # Main window attributes
@@ -7,39 +7,24 @@ def draw_window():
     window.title("Project Bunneh")
     window.geometry("400x800")
 
+
     # Frame for Main Menu
     main_menu_frame = ctk.CTkFrame(window)
     main_menu_frame.pack(expand=True,fill="both")
 
-    text_box = ctk.CTkTextbox(main_menu_frame)
-    text_box.pack(expand=True,fill="both",)
+    label = ctk.CTkLabel(main_menu_frame,text="""Press New Game to start a fresh new game.\nPress Load Game to continue.\nPress Tutorial if you need help with terms.\nPress Credits if you want to see who worked on this project.\nPress Quit Game to exit the game.""")
+    label.pack(expand=True,fill="both",)
 
-    button1 = ctk.CTkButton(main_menu_frame, text="New Game",)
-    button1.pack(expand=True,fill="both", pady=1)
+    button1 = ctk.CTkButton(main_menu_frame, text="New Game",command=new_char_window,)
+    button1.pack(expand=True,fill="both", pady=5,padx=5)
     button2 = ctk.CTkButton(main_menu_frame, text="Load Game",)
-    button2.pack(expand=True,fill="both", pady=1)
+    button2.pack(expand=True,fill="both", pady=5,padx=5)
     button3 = ctk.CTkButton(main_menu_frame, text="Tutorial",)
-    button3.pack(expand=True,fill="both", pady=1)
+    button3.pack(expand=True,fill="both", pady=5,padx=5)
     button4 = ctk.CTkButton(main_menu_frame, text="Credits",)
-    button4.pack(expand=True,fill="both", pady=1)
-    button5 = ctk.CTkButton(main_menu_frame, text="Quit Game",)
-    button5.pack(expand=True,fill="both", pady=1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    button4.pack(expand=True,fill="both", pady=5,padx=5)
+    button5 = ctk.CTkButton(main_menu_frame, text="Quit Game",command=window.destroy,)
+    button5.pack(expand=True,fill="both", pady=5,padx=5)
 
     # Keeping window open
     window.mainloop()
