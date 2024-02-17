ab2485e288689d44f64a4297476bad2bbc72b842
Aaron Joseph Alderdyce <95669204+AaronAlderdyce@users.noreply.github.com>
2024-02-16 23:32:19 -0800

    testing and building other menu items

--- project_bunneh/gui_funcs.py
+++ project_bunneh/gui_funcs.py
@@ -18,7 +18,7 @@ def new_char_window():
     label = ctk.CTkLabel(char_frame,text="Create Your Character:")
     label.pack()
 
-    button1 = ctk.CTkButton(char_window, text="Gender")
+    button1 = ctk.CTkButton(char_window, text="Gender",)
     button1.pack(pady=5,padx=5)
     button2 = ctk.CTkButton(char_window, text="Race",)
     button2.pack(pady=5,padx=5)
@@ -26,7 +26,7 @@ def new_char_window():
     button3.pack(pady=5,padx=5)
     button4 = ctk.CTkButton(char_window, text="Stats",)
     button4.pack(pady=5,padx=5)
-    button5 = ctk.CTkButton(char_window, text="Skills")
+    button5 = ctk.CTkButton(char_window, text="Skills",)
     button5.pack(pady=5,padx=5)
     button6 = ctk.CTkButton(char_window, text="Back", command=char_window.destroy)
     button6.pack(pady=5,padx=5)
