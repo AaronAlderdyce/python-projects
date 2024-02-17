b4ba0c4597751669908a3052660fbde70be0901f
Aaron Joseph Alderdyce <95669204+AaronAlderdyce@users.noreply.github.com>
2024-02-17 06:25:20 -0800

    pushing

--- project_bunneh/gui_funcs.py
+++ project_bunneh/gui_funcs.py
@@ -1,5 +1,6 @@
 import os
 import customtkinter as ctk
+import tkinter as tk
 # from race import Race
 # from job import Job
 # from stats import Stats, DerivedStats
@@ -10,9 +11,11 @@ def new_char_window():
     char_window.title("Character Creation")
     char_window.geometry("800x800")
 
+
     # Frame for Char Creation
     char_frame = ctk.CTkFrame(char_window)
     char_frame.pack(expand=True,fill="both")
+
 
     # Add buttons and widgets for char creation
     label = ctk.CTkLabel(char_frame,text="Create Your Character:")
