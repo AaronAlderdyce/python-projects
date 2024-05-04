75bef2597594f348bd0faeada356a348743698af
Aaron Joseph Alderdyce <95669204+AaronAlderdyce@users.noreply.github.com>
2024-05-03 17:23:36 -0700

    fixed button color

--- project_bunneh/gui_funcs.py
+++ project_bunneh/gui_funcs.py
@@ -2,13 +2,10 @@ import tkinter as tk
 from gui_two import gui_two as g2
 
 
-
 def open_main_window():
     mw = tk.Tk()
     mw.title("Project Bunneh: Way of the Rabbit's Foot")
     mw.geometry("800x800")
-
-
 
     # Define custom colors
     background_color = "#336699"
@@ -19,18 +16,13 @@ def open_main_window():
     mw.configure(bg=background_color)
 
     # Create a label with custom text color
-    label = tk.Label(mw, text="Custom Tkinter Example", fg=text_color, bg=background_color)
+    label = tk.Label(mw, text="Custom Tkinter Example",
+                     fg=text_color, bg=background_color)
     label.pack(pady=10)
 
     # Create a button with custom background color
     # and to bring up gui_two
-    button = tk.Button(text="GUI_Two", command=lambda: g2()).pack()
-
-
-
-
-
-
-
+    button = tk.Button(text="GUI_Two", bg=button_color,
+                       command=lambda: g2()).pack()
 
     mw.mainloop()
